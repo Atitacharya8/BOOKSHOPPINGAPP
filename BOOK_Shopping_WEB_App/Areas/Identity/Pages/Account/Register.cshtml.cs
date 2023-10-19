1d6016b95b54a5e11dfd7dc803e0ac934ebc52df
Atit Acharya <atitacharya8@gmail.com>
2023-10-19 11:15:28 -0500

    Display Company Dropdown

--- BOOK_Shopping_WEB_App/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BOOK_Shopping_WEB_App/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using BOOKSHOPPING.DataAccess.Repository.IRepository;
 using BOOKSHOPPING.Models;
 using BOOKSHOPPING.Utility;
 using Microsoft.AspNetCore.Authentication;
@@ -34,6 +35,7 @@ namespace BOOK_Shopping_WEB_App.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly IUnitOfWork _unitOfWork;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
@@ -41,7 +43,8 @@ namespace BOOK_Shopping_WEB_App.Areas.Identity.Pages.Account
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            IUnitOfWork unitOfWork)
         {
             _userManager = userManager;
             _roleManager = roleManager;
@@ -50,6 +53,7 @@ namespace BOOK_Shopping_WEB_App.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _unitOfWork = unitOfWork;
         }
 
         /// <summary>
@@ -117,6 +121,9 @@ namespace BOOK_Shopping_WEB_App.Areas.Identity.Pages.Account
             public string? State { get; set; }
             public string? PostalCode { get; set; }
             public string? PhoneNumber { get; set; }
+            public int? CompanyId { get; set; }
+            [ValidateNever]
+            public IEnumerable<SelectListItem> CompanyList { get; set; }
 
         }
 
@@ -137,6 +144,12 @@ namespace BOOK_Shopping_WEB_App.Areas.Identity.Pages.Account
                 {
                     Text = i,
                     Value = i
+                }),
+
+                CompanyList = _unitOfWork.Company.GetAll().Select(u=> new SelectListItem 
+                {
+                    Text = u.Name,
+                   Value = u.Id.ToString()
                 })
             };
 
