d77939b1bf0cba22eb3c93d03d546b2057566c78
Atit Acharya <atitacharya8@gmail.com>
2023-10-19 22:18:42 -0500

    Get Order total in shopping cart Logic in cart controller

--- BOOK_Shopping_WEB_App/Areas/Customer/Controllers/CartController.cs
+++ BOOK_Shopping_WEB_App/Areas/Customer/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 ﻿using BOOKSHOPPING.DataAccess.Repository.IRepository;
+using BOOKSHOPPING.Models;
 using BOOKSHOPPING.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -26,8 +27,33 @@ namespace BOOK_Shopping_WEB_App.Areas.Customer.Controllers
             { 
                    ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product")
             };
+
+            foreach(var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                double price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartVM.OrderTotal += (price * cart.Count);
+            }
                 
             return View(ShoppingCartVM);
+        }
+
+        private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
+        {
+            if (shoppingCart.Count <= 50)
+            {
+                return shoppingCart.Product.Price;
+            }
+            else
+            {
+                if (shoppingCart.Count <= 100)
+                {
+                    return shoppingCart.Product.Price50;
+                }
+                else
+                {
+                    return shoppingCart.Product.Price100;
+                }
+            }
         }
     }
 }
