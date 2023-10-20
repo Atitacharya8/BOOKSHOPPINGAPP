b0ad49d0f4a03222b2c76b84bc68f2bd2f4d65da
Atit Acharya <atitacharya8@gmail.com>
2023-10-19 21:53:35 -0500

    Shopping Cart Controller and UI Added

---
+++ BOOK_Shopping_WEB_App/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BOOK_Shopping_WEB_App.Areas.Customer.Controllers
+{
+    [Area("customer")]
+    public class CartController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
