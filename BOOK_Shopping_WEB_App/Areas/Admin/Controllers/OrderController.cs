24097297bff8f6899546ed8e528fbfafab6fa4b0
atitacharya8 <atitacharya8@gmail.com>
2023-10-28 23:18:14 -0500

    Demo - Filters in Order List

--- BOOK_Shopping_WEB_App/Areas/Admin/Controllers/OrderController.cs
+++ BOOK_Shopping_WEB_App/Areas/Admin/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using BOOKSHOPPING.DataAccess.Repository.IRepository;
 using BOOKSHOPPING.Models;
+using BOOKSHOPPING.Utility;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BOOK_Shopping_WEB_App.Areas.Admin.Controllers
@@ -22,10 +23,29 @@ namespace BOOK_Shopping_WEB_App.Areas.Admin.Controllers
 		#region API CALLS
 		[HttpGet]
 
-		public IActionResult GetAll() 
+		public IActionResult GetAll(string status) 
 		{
-			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
-			return Json(new { data = objOrderHeaders });
+			IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+
+            }
+            return Json(new { data = objOrderHeaders });
 		}
 
 		#endregion
