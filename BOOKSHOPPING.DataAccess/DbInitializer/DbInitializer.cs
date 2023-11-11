5cac8ff56dae4197ab4d6a9212c3ca579f65a89c
atitacharya8 <atitacharya8@gmail.com>
2023-11-10 22:32:20 -0600

    IDbInitializer

---
+++ BOOKSHOPPING.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOOKSHOPPING.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
